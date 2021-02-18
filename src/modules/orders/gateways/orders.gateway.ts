18308c63df4d5bba16d10880130b178dca49d8d0
cristian stiven p <styven21121@gmail.com>
2021-02-17 22:37:57 -0500

    listen clients connections

--- src/modules/orders/gateways/orders.gateway.ts
+++ src/modules/orders/gateways/orders.gateway.ts
@@ -1,16 +1,27 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { OnEvent } from '@nestjs/event-emitter';
 import * as Ws from 'ws';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway()
-export class OrdersGateway {
+export class OrdersGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Ws.Server;
+
+  handleConnection(client: any, ...args: any[]): any {
+    Logger.log('New Client Connected', 'OrdersGateway');
+  }
+
+  handleDisconnect(client: any): any {
+    Logger.log('Client Disconnected Connected', 'OrdersGateway');
+  }
 
   @SubscribeMessage('newOrder')
   handleMessage(@MessageBody() data: string): string {
