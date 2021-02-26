c2291facb6c73207dad3e5beff930d8550f7396b
cristian stiven p <styven21121@gmail.com>
2021-02-25 22:05:19 -0500

    remove JwtModule from Orders module

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -4,21 +4,9 @@ import { OrdersService } from '@modules/orders/services/orders/orders.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from '@modules/orders/entities/order.entity';
 import { OrdersGateway } from '@modules/orders/gateways/orders.gateway';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { CONFIG } from '@config/config-keys';
 
 @Module({
   imports: [
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get(CONFIG.JWT_SECRET_KEY),
-        signOptions: {
-          expiresIn: configService.get(CONFIG.JWT_EXPIRATION_TIME),
-        },
-      }),
-    }),
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
